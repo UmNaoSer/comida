baab5d8a5ce892c2f042457189588f942d2ff926
UmNãoSer <umnaoser@gmail.com>
2026-02-28 21:57:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8659990442-d5739",
+  "appId": "1:694830726711:web:ffa9ccb8523adc6b7b89b2",
+  "apiKey": "AIzaSyCiFy0vbva5taGeYsvuOXkSsRhO-gshAig",
+  "authDomain": "studio-8659990442-d5739.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "694830726711"
+};
